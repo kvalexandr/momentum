e9126ae424664c64aaae26cccf8425ed05833373
Alexandr <k.v.alexandr@gmail.com>
2020-10-25 00:58:25 +0400

    feat: add background

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -39,3 +39,65 @@ const showDay = () => {
 
   day.innerHTML = today.toLocaleString("en-US", options);
 };
+
+// background
+
+const randomArrayImageIndex = () => Array(20).fill(0).map((e, i) => (i + 1)).sort(() => .5 - Math.random()).slice(0, 6);
+const arrDayBg = [];
+arrDayBg.push(...randomArrayImageIndex());
+arrDayBg.push(...randomArrayImageIndex());
+arrDayBg.push(...randomArrayImageIndex());
+arrDayBg.push(...randomArrayImageIndex());
+
+const getDay = (h) => {
+  if (h >= 0 && h < 6) {
+    return 'night';
+  }
+
+  if (h >= 6 && h < 12) {
+    return 'morning';
+  }
+
+  if (h >= 12 && h < 18) {
+    return 'day';
+  }
+
+  if (h >= 18 && h <= 23) {
+    return 'evening';
+  }
+}
+
+const setBgGreet = () => {
+  const today = new Date();
+  const hour = today.getHours();
+  const day = getDay(hour);
+
+  viewBgImage(day, hour);
+
+  switch (day) {
+    case 'night':
+      greeting.innerHTML = 'Good night, ';
+      return;
+    case 'morning':
+      greeting.innerHTML = 'Good morning, ';
+      return;
+    case 'day':
+      greeting.innerHTML = 'Good day, ';
+      return;
+    case 'evening':
+      greeting.innerHTML = 'Good evening, ';
+      return;
+  }
+};
+
+const viewBgImage = (day, imageIndex) => {
+  const body = document.querySelector('body');
+  const img = document.createElement('img');
+  const src = `/assets/images/${day}/${addZero(arrDayBg[imageIndex])}.jpg`
+  img.src = src;
+  btnBg.disabled = true;
+  img.onload = () => {
+    body.style.backgroundImage = `url(${src})`;
+    setTimeout(function () { btnBg.disabled = false; }, 1000);
+  };
+};
